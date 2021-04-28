e619dfdfa037c29f4932b66c00c2b91d3bbf7488
MikoloD <61506836+MikoloD@users.noreply.github.com>
2021-04-28 22:54:02 +0200

    Adding Carusel

--- InterstPage/InterstPage/Controllers/HomeController.cs
+++ InterstPage/InterstPage/Controllers/HomeController.cs
@@ -11,7 +11,19 @@ namespace InterstPage.Controllers
 {
     public class HomeController : Controller
     {
+        private List<PhotoModel> CreateModel()
+        {
+            List<PhotoModel> IntrestList = new List<PhotoModel>()
+            {
+                new PhotoModel {URL="https://www.muzykotekaszkolna.pl/!data/resources/graphics/co_slychac/Teatr_Wielki_ok.1900.jpg",PhotoDesccription="Architektura"},
+                new PhotoModel {URL="https://e-shareef.com/images/portfolio/portfolio-08-large.jpg",PhotoDesccription="Programowanie"},
+                new PhotoModel {URL="https://wb713.files.wordpress.com/2015/12/img.jpg",PhotoDesccription="Matematyka"},
+                new PhotoModel {URL="https://www.zachod24.pl/wp-content/uploads/sites/5/2020/11/rzad-1918-2020-11-10_13-44-08_611799.jpg",PhotoDesccription="Historia"}
+            };
+            return IntrestList;
+        }
         private readonly ILogger<HomeController> _logger;
+        private InterstModel model = new InterstModel();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,7 +32,8 @@ namespace InterstPage.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            model.MyInterstsList = CreateModel();
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ InterstPage/InterstPage/Models/PhotoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterstPage.Models
+{
+    public class PhotoModel
+    {
+        public string URL { get; set; }
+        public string PhotoDesccription { get; set; }
+        public string LongerDescrition { get; set; }
+    }
+}
