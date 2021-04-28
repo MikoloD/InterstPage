93931337947119404cc007e616ef485e4b0b089c
MikoloD <61506836+MikoloD@users.noreply.github.com>
2021-04-28 20:42:10 +0200

    Project Start

---
+++ InterstPage/InterstPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InterstPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
